4619c8e1c68e6a3dd12ec88a2fb2afdf6f854d76
quyettc93 <quyettc93@gmail.com>
2025-01-03 16:03:14 +0700

    truoc khi lam bai tap

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MealsOverview from "./screens/MealsOverview";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -30,7 +30,16 @@ export default function App() {
               title: "All Categories",
             }}
           />
-          <Stack.Screen name="MealsOverView" component={MealsOverview} />
+          <Stack.Screen
+            name="MealsOverView"
+            component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId;
+            //   return {
+            //     title: catId,
+            //   };
+            // }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,25 @@
+import { useLayoutEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import MealItem from "../components/MealItem";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
-function MealsOverview({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
   // loc ra neu categoryIds cua 1 item nao do co index >= 0 (khi ko tim that ra thi tra ve -1 nen retun la dieu kien loc)
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+  //dungf useLayoutEffect  de thay doi title cua navigation (layout de muot hon)
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    console.log("categoryTitle: ", categoryTitle);
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
   function renderMealItem(itemData) {
     const item = itemData.item;
     const mealItemProps = {
@@ -29,7 +41,7 @@ function MealsOverview({ route }) {
     </View>
   );
 }
-export default MealsOverview;
+export default MealsOverviewScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
